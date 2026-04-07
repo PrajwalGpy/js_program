26cb82f805917b718f4ed2419298c36727802535
PrajwalGpy <prajwalgpy@gmail.com>
2026-04-07 21:24:27 +0530

    first commit

---
+++ pro1.js
@@ -0,0 +1,20 @@
+let name1 = 'PRajawal'
+
+console.log(`MY name is ${name1}`)
+
+
+const go = 22 > 21 ? "21 is bigger " : "Samall"
+console.log( go )
+
+const value = 'rajju'
+
+switch(value){
+    case 'pajju':
+        console.log("its pajju")
+        break
+    case 'rajju':
+        console.log('its rajju')
+        break
+    default:
+        console.log("ITs nothing")
+}
