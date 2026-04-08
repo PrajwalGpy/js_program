cd556cde38a289cff96d1f70376f8187d493d6c5
PrajwalGpy <prajwalgpy@gmail.com>
2026-04-08 06:51:50 +0530

    very well

--- pro1.js
+++ pro1.js
@@ -1,20 +1,26 @@
-let name1 = 'PRajawal'
+// let name1 = "PRajawal";
 
-console.log(`MY name is ${name1}`)
+// console.log(`MY name is ${name1}`);
 
+// const go = 22 > 21 ? "21 is bigger " : "Samall";
+// console.log(go);
 
-const go = 22 > 21 ? "21 is bigger " : "Samall"
-console.log( go )
+// const value = "rajju";
 
-const value = 'rajju'
+// switch (value) {
+//   case "pajju":
+//     console.log("its pajju");
+//     break;
+//   case "rajju":
+//     console.log("its rajju");
+//     break;
+//   default:
+//     console.log("ITs nothing");
+// }
 
-switch(value){
-    case 'pajju':
-        console.log("its pajju")
-        break
-    case 'rajju':
-        console.log('its rajju')
-        break
-    default:
-        console.log("ITs nothing")
-}
+// let page = [10,20,30,40,50]
+
+// let go = page.forEach((element) => {
+//   console.log(element)
+// });
+
